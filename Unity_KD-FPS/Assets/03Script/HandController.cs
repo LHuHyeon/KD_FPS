5b57c8c9936ab6a8dd78f21b75a03f63e8c218ef
LHH <101979673+LeeHuHyeon@users.noreply.github.com>
2022-08-24 15:00:55 +0900

    유니티 KD_FPS 폴더 업로드
    내가 현재 공부중인 강좌[케이디]를 깃허브에 첫 연동!

---
+++ Unity_KD-FPS/Assets/03Script/HandController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    // 현재 장착된 Hand형 타입 무기.
+    [SerializeField]
+    private Hand currentHand;
+    
+    // 공격중?
+    private bool isAttack = false;
+    private bool isSwing = false;
+
+    private RaycastHit hitInfo;
+
+    void Update()
+    {
+        TryAttack();
+    }
+
+    // 공격 여부 확인
+    private void TryAttack()
+    {
+        if (Input.GetButton("Fire1")){
+            if (!isAttack){
+                // 코루틴 실행
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    // 공격 진행 코루틴
+    IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+
+        currentHand.anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(currentHand.attackDelayA);
+        isSwing = true;
+
+        // 공격 활성화 시점.
+        StartCoroutine(HitCoroutine());
+
+        yield return new WaitForSeconds(currentHand.attackDelayB);
+        isSwing = false;
+
+        yield return new WaitForSeconds(currentHand.attackDelay - currentHand.attackDelayA - currentHand.attackDelayB);
+
+        isAttack = false;
+    }
+
+    // 피격 여부 코루틴
+    IEnumerator HitCoroutine()
+    {
+        while (isSwing){
+            if (CheckObject()){
+                isSwing = false;
+                Debug.Log(hitInfo.transform.name);
+            }
+            yield return null;
+        }
+    }
+
+    // Raycast를 통한 피격 확인
+    private bool CheckObject()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, currentHand.range)){
+            return true;
+        }
+        return false;
+    }
+}
