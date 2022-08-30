ff3b1a068e930d7209a8ccc18d67615bf63cccb8
LHuHyeon <minfkffn@gmail.com>
2022-08-30 17:11:19 +0900

    아이템 흭득 스크립트 추가
    케이디 Part 14 강좌

--- Unity_KD-FPS/Assets/03Script/Rock.cs
+++ Unity_KD-FPS/Assets/03Script/Rock.cs
@@ -20,6 +20,11 @@ public class Rock : MonoBehaviour
     private GameObject go_debris;   // 깨진 바위
     [SerializeField]
     private GameObject go_effect_prefab;    // 채굴 이팩트
+    [SerializeField]
+    private GameObject go_rock_item_prefab; // 돌맹이 아이템
+
+    // 돌맹이 아이템 등장 개수
+    private int count;
 
     // 필요한 사운드 이름
     [SerializeField]
@@ -27,6 +32,11 @@ public class Rock : MonoBehaviour
     [SerializeField]
     private string destroy_Sound;
 
+    private void Start() {
+        count = Random.Range(2, 5); // 돌맹이 등장 개수 설정
+    }
+
+    // 데미지를 입을 때
     public void Mining()
     {
         SoundManager.instance.PlaySE(strike_Sound);
@@ -40,11 +50,19 @@ public class Rock : MonoBehaviour
         }
     }
 
+    // 부서졌을 때
     private void Destruction()
     {
         SoundManager.instance.PlaySE(destroy_Sound);
 
         col.enabled = false;
+        // 돌맹이 소환
+        for (int i = 1; i <= count; i++){
+            Instantiate(go_rock_item_prefab, 
+                        new Vector3(go_rock.transform.position.x, go_rock.transform.position.y+0.5f, go_rock.transform.position.z),
+                        Quaternion.identity);
+        }
+        
         Destroy(go_rock);
 
         go_debris.SetActive(true);
