a9f6c3e06c74f73bd937625aaabb26a6dfdb1a07
LHuHyeon <minfkffn@gmail.com>
2022-09-06 16:03:41 +0900

    건축 불가 구현
    [케이디 Part 26 강좌]
    1. 건축 프리뷰가 생성될 때 green/red 색으로 건축 유무를 확인할 수 있다.

--- Unity_KD-FPS/Assets/03Script/UI/CraftManual.cs
+++ Unity_KD-FPS/Assets/03Script/UI/CraftManual.cs
@@ -68,7 +68,7 @@ public class CraftManual : MonoBehaviour
     // 프리뷰 위치에 프리팹 생성
     private void Build()
     {
-        if (isPreviewActivated){
+        if (isPreviewActivated && go_Preview.GetComponent<PreviewObject>().isBuildable()){
             Instantiate(go_Prefab, hitInfo.point, Quaternion.identity);
             Destroy(go_Preview);
             isActivated = false;
